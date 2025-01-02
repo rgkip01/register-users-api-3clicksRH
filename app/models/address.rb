41fb83f6b0d971e51410c5e18c451b8a39d2abb4
@rafaabreu <rafaabreu@prosas.com.br>
2025-01-02 20:19:09 -0300

    [feat]: add validation in models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :street,
+            :city,
+            :state,
+            :zip_code,
+            :country, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :addresses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :document, presence: true, uniqueness: true, format: { with: /\A\d{11}\z/, message: 'deve conter 11 dígitos.' }
+  validates :date_of_birth, presence: true
 end
